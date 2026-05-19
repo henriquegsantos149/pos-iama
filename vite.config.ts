a9bfe724aeb91c4e7f79aa7da357718c341742ac
Olivio <edvaldoolivio3@gmail.com>
2026-05-19 15:48:08 -0300

    build: set vite base path to /pos-iama/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/pos-iama/',
   plugins: [react(), tailwindcss()],
 })
 
